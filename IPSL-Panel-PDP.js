d0dc127500fb1af6acdc32dbc0a95a78eac7a9c9
Venask4 <mgcoloe@gmail.com>
2017-05-24 20:57:23 -0400

    Change prod info box

--- IPSL-Panel-PDP.js
+++ IPSL-Panel-PDP.js
@@ -23,7 +23,8 @@ var exp = (function($) {
 	// Variables
 	// Object containing variables, generally these would be strings or jQuery objects
 	exp.vars = {
-		hyperCatInfo: '<div class="AWA-cat-info"><p>Panels are 2.5mm thick x 1222mm wide</p><p>Length: 2440 mm or 3050mm</p><p>Solid uPVC</p><p>Guide price: £17.72/m2</p></div><p class="AWA-finish">Satin Finish</p>'
+		hyperCatInfo: '<div class="AWA-cat-info"><p>Panels are 2.5mm thick x 1222mm wide</p><p>Length: 2440 mm or 3050mm</p><p>Solid uPVC</p><p>Guide price: £17.72/m2</p></div><div class="AWA-quote-div"><a href="https://www.ipsluk.co.uk/customer-service/contact-us-free-brochure.html"><img class="image-padding" src="http://useruploads.visualwebsiteoptimizer.com/useruploads/268527/images/8e9c271e84e7d4355b25ee8fbbf3870e_btn-requestquote.png"></a><p class="bar-phone"> <span class="phonetext">Help &amp; Advice: <br><i class="help-icon"></i><span data-calltracks-replaced-at="1486964770862" data-calltracks-orig-innerhtml="0114%20332%200388" onclick="window.location="tel:"+this.innerHTML;" class="calltracks_ipslukcouk-main">0114 332 0669</span> </span></p></div><p class="AWA-finish">Satin Finish</p>',
+		getQuoteButton: '<div class="AWA-quote-div"><a href="https://www.ipsluk.co.uk/customer-service/contact-us-free-brochure.html"><img class="image-padding" src="http://useruploads.visualwebsiteoptimizer.com/useruploads/268527/images/8e9c271e84e7d4355b25ee8fbbf3870e_btn-requestquote.png"></a><p class="bar-phone"> <span class="phonetext">Help &amp; Advice:<i class="help-icon"></i><span data-calltracks-replaced-at="1486964770862" data-calltracks-orig-innerhtml="0114%20332%200388" onclick="window.location="tel:"+this.innerHTML;" class="calltracks_ipslukcouk-main">0114 332 0669</span> </span></p></div>'
 		};
 
 	// Styles
@@ -37,6 +38,9 @@ var exp = (function($) {
 		}\
 		.imgbox1.ipad {\
 			float: left;\
+			position: absolute;\
+			z-index: 0;\
+			margin-left: 5%;\
 		}\
 		.inner-banner .BNR-bottom {\
 			height: auto;\
@@ -51,12 +55,38 @@ var exp = (function($) {
 			clear: both;\
 		}\
 		.AWA-cat-info {\
-			float: left;\
-			margin-top: 16px;\
+			padding-top: 16px;\
+			display: inline-block;\
+			margin-left: 520px;\
+			z-index: 1;\
+			position: relative;\
 		}\
 		.AWA-cat-info p {\
 			color: black;\
 			font-weight: bold;\
+		}\
+		.AWA-quote-div {\
+			display: inline-block;\
+			margin-left: 5%;\
+			vertical-align: top;\
+    		margin-top: 50px;\
+		}\
+		.AWA-quote-div .phonetext {\
+			color: black;\
+			font-weight: bold;\
+		}\
+		.AWA-quote-div .help-icon {\
+			background: hsla(0, 0%, 0%, 0) url(https://www.ipsluk.co.uk/skin/frontend/rwd/default/images/telephone-white.svg) no-repeat scroll 0 0;\
+			height: 20px;\
+			display: inline-block;\
+			margin: 0 0 0 0 !important;\
+		}\
+		.col2-left-layout .col-main {\
+			float: none;\
+			margin: 0 auto;\
+		}\
+		.col-main {\
+			float: none;\
 		}\
 	';
 
@@ -69,9 +99,14 @@ var exp = (function($) {
 
 		// PROCLAD HYPER GRADE
 		if (window.location.href.indexOf('proclad-hyper-grade') > -1 ) {
+			
+			// Add in additional product info
 			var $imgbox1 = $('.imgbox1.ipad');
 			$imgbox1.after(exp.vars.hyperCatInfo);
 
+			// Hide value ticks and random testimonials
+			$('.advive-link.lefts').hide();
+			$('.col-left.sidebar.col-left-first').hide();
 		}
 
 	};
